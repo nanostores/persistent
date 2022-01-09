ec863551fdfbcf46af12b9c090d293f3bfa9c3c2
Andrey Sitnik <andrey@sitnik.ru>
2022-01-09 18:27:53 +0100

    Fix test names

--- test/ssr.test.ts
+++ test/ssr.test.ts
@@ -3,7 +3,7 @@ import { test } from 'uvu'
 
 import { persistentAtom, persistentMap } from '../index.js'
 
-test('works without localStorage', () => {
+test('works without localStorage for map', () => {
   let map = persistentMap<{ one?: string; two?: string }>('a:', {
     one: '1'
   })
@@ -12,7 +12,7 @@ test('works without localStorage', () => {
   equal(map.get(), { one: '1', two: '2' })
 })
 
-test('works without localStorage', () => {
+test('works without localStorage for atom', () => {
   let store = persistentAtom<string>('a', '1')
   store.listen(() => {})
   store.set('2')
