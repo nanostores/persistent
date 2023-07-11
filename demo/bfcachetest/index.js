60541344bebe73afc6488c3ee8ea7eb3729a17f3
GoldStrikeArch <arthasmontano@mail.ru>
2023-07-11 16:27:54 +0300

    Removed trailing semicolon (PR review)

--- demo/bfcachetest/index.js
+++ demo/bfcachetest/index.js
@@ -1,34 +1,34 @@
-import { anAtom, aMap } from "../index.js";
+import { anAtom, aMap } from "../index.js"
 
-const preAtom = document.querySelector("#atom");
-const preMap = document.querySelector("#map");
+const preAtom = document.querySelector("#atom")
+const preMap = document.querySelector("#map")
 
-const buttonAtom = document.querySelector("#buttonAtom");
-const clearButtonAtom = document.querySelector("#clearAtom");
+const buttonAtom = document.querySelector("#buttonAtom")
+const clearButtonAtom = document.querySelector("#clearAtom")
 
-const buttonMap = document.querySelector("#buttonMap");
-const clearButtonMap = document.querySelector("#clearMap");
+const buttonMap = document.querySelector("#buttonMap")
+const clearButtonMap = document.querySelector("#clearMap")
 
 buttonAtom.addEventListener("click", () => {
-  anAtom.set([...anAtom.get(), Math.random() * Date.now()]);
-});
+  anAtom.set([...anAtom.get(), Math.random() * Date.now()])
+})
 
 clearButtonAtom.addEventListener("click", () => {
-  anAtom.set([]);
-});
+  anAtom.set([])
+})
 
 anAtom.subscribe((val) => {
-  preAtom.textContent = JSON.stringify(val, null, 2);
-});
+  preAtom.textContent = JSON.stringify(val, null, 2)
+})
 
 buttonMap.addEventListener("click", () => {
-  aMap.set({ ...aMap.get(), a: String(Math.random() * Date.now()) });
-});
+  aMap.set({ ...aMap.get(), a: String(Math.random() * Date.now()) })
+})
 
 clearButtonMap.addEventListener("click", () => {
-  aMap.set({});
-});
+  aMap.set({})
+})
 
 aMap.subscribe((val) => {
-  preMap.textContent = JSON.stringify(val, null, 2);
-});
+  preMap.textContent = JSON.stringify(val, null, 2)
+})
--- index.js
+++ index.js
@@ -1,200 +1,200 @@
-import { map, atom, onMount } from "nanostores";
+import { map, atom, onMount } from "nanostores"
 
-let identity = (a) => a;
-let storageEngine = {};
-let eventsEngine = { addEventListener() {}, removeEventListener() {} };
+let identity = (a) => a
+let storageEngine = {}
+let eventsEngine = { addEventListener() {}, removeEventListener() {} }
 
 function testSupport() {
   try {
-    return typeof localStorage !== "undefined";
+    return typeof localStorage !== "undefined"
   } catch {
     /* c8 ignore next 3 */
     // In Privacy Mode access to localStorage will return error
-    return false;
+    return false
   }
 }
 if (testSupport()) {
-  storageEngine = localStorage;
+  storageEngine = localStorage
 }
 
-let pageShowHandler;
-const backFromBfCache = (sync) => () => sync();
+let pageShowHandler
+const backFromBfCache = (sync) => () => sync()
 
 export let windowPersistentEvents = {
   addEventListener(key, listener, sync) {
-    pageShowHandler = backFromBfCache(sync);
+    pageShowHandler = backFromBfCache(sync)
 
-    window.addEventListener("storage", listener);
-    window.addEventListener("pageshow", pageShowHandler);
+    window.addEventListener("storage", listener)
+    window.addEventListener("pageshow", pageShowHandler)
   },
   removeEventListener(key, listener) {
-    window.removeEventListener("storage", listener);
-    window.removeEventListener("pageshow", pageShowHandler);
+    window.removeEventListener("storage", listener)
+    window.removeEventListener("pageshow", pageShowHandler)
   },
-};
+}
 
 if (typeof window !== "undefined") {
-  eventsEngine = windowPersistentEvents;
+  eventsEngine = windowPersistentEvents
 }
 
 export function setPersistentEngine(storage, events) {
-  storageEngine = storage;
-  eventsEngine = events;
+  storageEngine = storage
+  eventsEngine = events
 }
 
 export function persistentAtom(name, initial = undefined, opts = {}) {
-  let encode = opts.encode || identity;
-  let decode = opts.decode || identity;
+  let encode = opts.encode || identity
+  let decode = opts.decode || identity
 
-  let store = atom(initial);
+  let store = atom(initial)
 
-  let set = store.set;
+  let set = store.set
   store.set = (newValue) => {
     if (typeof newValue === "undefined") {
-      delete storageEngine[name];
+      delete storageEngine[name]
     } else {
-      storageEngine[name] = encode(newValue);
+      storageEngine[name] = encode(newValue)
     }
-    set(newValue);
-  };
+    set(newValue)
+  }
 
   function listener(e) {
     if (e.key === name) {
       if (e.newValue === null) {
-        set(undefined);
+        set(undefined)
       } else {
-        set(decode(e.newValue));
+        set(decode(e.newValue))
       }
     } else if (!storageEngine[name]) {
-      set(undefined);
+      set(undefined)
     }
   }
 
   onMount(store, () => {
     let sync = () => {
-      store.set(storageEngine[name] ? decode(storageEngine[name]) : initial);
-    };
-
-    sync();
-    if (opts.listen !== false) {
-      eventsEngine.addEventListener(name, listener, sync);
-      return () => {
-        eventsEngine.removeEventListener(name, listener);
-      };
+      store.set(storageEngine[name] ? decode(storageEngine[name]) : initial)
     }
-  });
 
-  return store;
+    sync()
+    if (opts.listen !== false) {
+      eventsEngine.addEventListener(name, listener, sync)
+      return () => {
+        eventsEngine.removeEventListener(name, listener)
+      }
+    }
+  })
+
+  return store
 }
 
 export function persistentMap(prefix, initial = {}, opts = {}) {
-  let encode = opts.encode || identity;
-  let decode = opts.decode || identity;
+  let encode = opts.encode || identity
+  let decode = opts.decode || identity
 
-  let store = map();
+  let store = map()
 
-  let setKey = store.setKey;
+  let setKey = store.setKey
   store.setKey = (key, newValue) => {
     if (typeof newValue === "undefined") {
       if (opts.listen !== false && eventsEngine.perKey) {
-        eventsEngine.removeEventListener(prefix + key, listener);
+        eventsEngine.removeEventListener(prefix + key, listener)
       }
-      delete storageEngine[prefix + key];
+      delete storageEngine[prefix + key]
     } else {
       if (
         opts.listen !== false &&
         eventsEngine.perKey &&
         !(key in store.value)
       ) {
-        eventsEngine.addEventListener(prefix + key, listener);
+        eventsEngine.addEventListener(prefix + key, listener)
       }
-      storageEngine[prefix + key] = encode(newValue);
+      storageEngine[prefix + key] = encode(newValue)
     }
-    setKey(key, newValue);
-  };
+    setKey(key, newValue)
+  }
 
-  let set = store.set;
+  let set = store.set
   store.set = function (newObject) {
     for (let key in newObject) {
-      store.setKey(key, newObject[key]);
+      store.setKey(key, newObject[key])
     }
     for (let key in store.value) {
       if (!(key in newObject)) {
-        store.setKey(key);
+        store.setKey(key)
       }
     }
-  };
+  }
 
   function listener(e) {
     if (!e.key) {
-      set({});
+      set({})
     } else if (e.key.startsWith(prefix)) {
       if (e.newValue === null) {
-        setKey(e.key.slice(prefix.length), undefined);
+        setKey(e.key.slice(prefix.length), undefined)
       } else {
-        setKey(e.key.slice(prefix.length), decode(e.newValue));
+        setKey(e.key.slice(prefix.length), decode(e.newValue))
       }
     }
   }
 
   onMount(store, () => {
     let sync = () => {
-      let data = { ...initial };
+      let data = { ...initial }
       for (let key in storageEngine) {
         if (key.startsWith(prefix)) {
-          data[key.slice(prefix.length)] = decode(storageEngine[key]);
+          data[key.slice(prefix.length)] = decode(storageEngine[key])
         }
       }
-      store.set(data);
-    };
-
-    sync();
-    if (opts.listen !== false) {
-      eventsEngine.addEventListener(prefix, listener, sync);
-      return () => {
-        eventsEngine.removeEventListener(prefix, listener);
-        for (let key in store.value) {
-          eventsEngine.removeEventListener(prefix + key, listener);
-        }
-      };
+      store.set(data)
     }
-  });
 
-  return store;
+    sync()
+    if (opts.listen !== false) {
+      eventsEngine.addEventListener(prefix, listener, sync)
+      return () => {
+        eventsEngine.removeEventListener(prefix, listener)
+        for (let key in store.value) {
+          eventsEngine.removeEventListener(prefix + key, listener)
+        }
+      }
+    }
+  })
+
+  return store
 }
 
-let testStorage = {};
-let testListeners = [];
+let testStorage = {}
+let testListeners = []
 
 export function useTestStorageEngine() {
   setPersistentEngine(testStorage, {
     addEventListener(key, cb) {
-      testListeners.push(cb);
+      testListeners.push(cb)
     },
     removeEventListener(key, cb) {
-      testListeners = testListeners.filter((i) => i !== cb);
+      testListeners = testListeners.filter((i) => i !== cb)
     },
-  });
+  })
 }
 
 export function setTestStorageKey(key, newValue) {
   if (typeof newValue === "undefined") {
-    delete testStorage[key];
+    delete testStorage[key]
   } else {
-    testStorage[key] = newValue;
+    testStorage[key] = newValue
   }
-  let event = { key, newValue };
+  let event = { key, newValue }
   for (let listener of testListeners) {
-    listener(event);
+    listener(event)
   }
 }
 
 export function getTestStorage() {
-  return testStorage;
+  return testStorage
 }
 
 export function cleanTestStorage() {
   for (let i in testStorage) {
-    setTestStorageKey(i, undefined);
+    setTestStorageKey(i, undefined)
   }
 }
